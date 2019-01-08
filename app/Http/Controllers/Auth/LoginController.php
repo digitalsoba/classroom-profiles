49e71725b8698ee82eab8a698e51ee9a5ca81d3e
mbendandour <mbenda.ndour@metalab.csun.edu>
2019-01-08 11:24:02 -0800

    add next-class blade file

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/next-class';
 
     /**
      * Create a new controller instance.
---
+++ resources/views/next-class.blade.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mbendandour
+ * Date: 1/3/19
+ * Time: 2:31 PM
+ */
+
+@extends("layout.app")
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="">
+                    <img src="https://cdn.metalab.csun.edu/classrooms/EU103/front.jpg" alt="Lights" style="width:100%">
+                    <div class="caption">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="">
+                    <img src="https://cdn.metalab.csun.edu/classrooms/EU103/exit.jpg" alt="Nature" style="width:100%">
+                    <div class="caption">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="">
+                    <img src="https://cdn.metalab.csun.edu/classrooms/EU103/media.jpg" alt="Fjords" style="width:100%">
+                    <div class="caption">
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+@endsection
