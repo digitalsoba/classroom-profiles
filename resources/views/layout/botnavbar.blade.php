399410a4f308b258e93ed58b6ff63da4313aac39
Andrew Grano <andrew.grano@metalab.csun.edu>
2019-01-23 15:24:14 -0800

    install metaphorv2

--- resources/views/layout/botnavbar.blade.php
+++ resources/views/layout/botnavbar.blade.php
@@ -1,8 +1,19 @@
+
+<ul class="nav nav-metaphor"> 
+    <li class="nav-item flex-fill text-center"> <a class="nav-link active" href="">Active</a> </li> 
+    <li class="nav-item flex-fill text-center"> <a class="nav-link" href="">Link</a> </li> 
+    <li class="nav-item flex-fill text-center"> <a class="nav-link" href="">Link</a> </li> 
+    <li class="nav-item flex-fill text-center"> <a class="nav-link" href="">Link</a> </li> 
+    <li class="nav-item flex-fill text-center"> <a class="nav-link" href="">Link</a> </li> 
+    <li class="nav-item flex-fill text-center"> <a class="nav-link" href="">Link</a> </li> 
+    <li class="nav-item flex-fill text-center"> <a class="nav-link" href="">Link</a> </li> 
+</ul>
+
 <div class="tab-container">
     <ul class="tabs cf">
 
         <li class="tab__list">
-            <a class="tab__link" href="/equip">Equipment</a>
+            <a class="tab__link" href="/equip">Equipment <i class="fas fa-arrow-right"></i></a>
         </li>
         <li class="tab__list">
             <a class="tab__link" href="images.html">Images</a>
