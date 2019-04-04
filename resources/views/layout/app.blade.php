2d82cca663ef4af8de48fca1128cd41fd1e7e4c0
Mohamed.O <37858811+alprince1991@users.noreply.github.com>
2019-04-04 02:45:04 -0700

    no message

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -41,7 +41,7 @@
 
     {{-- Font Awesome --}}
     <script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy"
-        crossorigin="anonymous"></script>z
+        crossorigin="anonymous"></script>
     {{-- Footer --}}
     @include("layout.footer")
 
