b3483eab82036570f922c42ccf862d5d8fc5c014
Jonathan Kennedy <jekennedy8@gmail.com>
2019-04-03 21:44:23 -0700

    Moves the search menu to the guest page

--- resources/views/layout/guestPage.blade.php
+++ resources/views/layout/guestPage.blade.php
@@ -17,13 +17,45 @@
     <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
             integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
             crossorigin=""></script>
+    <style>
+        .class-search
+        {
+            color: #DB363C;
+            position: fixed;
+            right: 5px;
+            top: 50px;
+            background-color: white;
+            border-radius: 20px;
+            padding: 10px 15px;
+        }
+        .class-search .class-search-inner
+        {
+            display: none;
+        }
+        .class-search input
+        {
+            border-radius: 8px;
+            width: 120px;
+        }
+        .class-search:hover .class-search-inner
+        {
+            display: inline;
+        }
+    </style>
 
 
 </head>
 <body>
 @include('layout.topnavbar')
 
+<div class="class-search">
 
+    <svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg><!-- <i class="fas fa-search"></i> -->
+    <div class="class-search-inner">
+        Enter a class<br>
+        <input type="search" placeholder="EX. JD2211">
+    </div>
+</div>
 
 
 @yield("content")
