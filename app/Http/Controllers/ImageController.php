21c6775fcde709d135a7b1e80ceb786ad07307fa
digitalsoba <anthony.inlavong@gmail.com>
2019-04-09 09:50:37 -0700

    Revert to 9cc155122d0ff85d0ad054bad45058c634d0e50b

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -21,7 +21,7 @@ class ImageController extends Controller {
 
     public function __construct()
     {
-        $this->middleware('auth');
+      //  $this->middleware('auth');
     }
 
 
--- resources/js/components/equipment.vue
+++ resources/js/components/equipment.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h2 class="text-center">Choose a Class</h2>
-      </div>
-    </div>
-    <div class="row text-center">
-      <div class="col">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3 class="text-left">Choose a Classroom</h3>
+                <h6 class="text-left">(View Equipment for the Classroom Selected)</h6>
+            </div>
+        </div>
+        <br>
+
         <form>
-          <div class="form-group">
-            <select id="list" accesskey="target" ref="selectField" class="form-control">
-              <option v-for="item in equip">{{item.Building_Room}}</option>
-            </select>
-          </div>
-          <button type="button" @click="goToNewPage()" class="btn btn-rounded btn-primary">View equipment</button><br>
+            <div class="form-group">
+                <select id="list" accesskey="target" ref="selectField" class="form-control">
+                    <option v-for="item in equip">{{item.Building_Room}}</option>
+                </select>
+            </div>
+            <br>
+            <button type="button" @click="goToNewPage()" class="btn btn-rounded btn-primary">View Equipment</button>
         </form>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  name: "equipment",
-  props: ["equip"],
-  computed: {},
-  methods: {
-    goToNewPage: function() {
-      console.log(this.$refs.selectField.value);
-      window.location = "equip/" + this.$refs.selectField.value;
-    }
-  }
-};
+    export default {
+        name: "equipment",
+        props: ["equip"],
+        computed: {},
+        methods: {
+            goToNewPage: function() {
+                console.log(this.$refs.selectField.value);
+                window.location = "equip/" + this.$refs.selectField.value;
+            }
+        }
+    };
 </script>
