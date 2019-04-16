09f7c1de9c76bcb0c08d43951c3523df6ce78ce7
Mohamed <37858811+alprince1991@users.noreply.github.com>
2019-04-16 10:27:19 -0700

    Cp 51 (#64)
    * Login modal
    
    * Merge branch 'dev' of https://github.com/CSUN-Comp490/classroom-profiles into CP-46
    
    # Conflicts:
    #	public/js/app.js
    #	resources/views/layout/app.blade.php
    #	resources/views/layout/topnavbar.blade.php
    
    * no message
    
    * no message
    
    * Merge branch 'dev' of https://github.com/CSUN-Comp490/classroom-profiles into dev
    
    # Conflicts:
    #	public/css/app.css
    #	resources/sass/app.scss
    
    * no message

--- resources/views/pages/image-room.blade.php
+++ resources/views/pages/image-room.blade.php
@@ -37,12 +37,13 @@
     <div class="container nav-fill">
         <ul class="nav nav-metaphor">
             <li class="nav-item flex-fill text-center "> <a class="nav-link  " href="/equip/{{$classroom}}">Equipment</a> </li>
-            <li class="nav-item flex-fill text-center"> <a class="nav-link active" href="/image/{{$classroom}}">Images</a> </li>
+            <li class="nav-item flex-fill text-center tab-content shadow"> <a class="nav-link active" href="/image/{{$classroom}}">Images</a> </li>
         </ul>
     </div>
     <div class="container">
     <div class="row">
         <div class="col">
+            <br>
             <h2 class="text-center">These are the images for the room: {{$classroom}}</h2>
         </div>
     </div>
@@ -177,5 +178,12 @@
             </div>
         </div>
     </div>
-
+<style>
+    .shadow {
+        -webkit-box-shadow: 0px 10px 4px -2px rgba(0,0,0,0.75);
+        -moz-box-shadow: 0px 10px 4px -2px rgba(0,0,0,0.75);
+        box-shadow: 0px 10px 4px -2px rgba(0,0,0,0.75);
+    }
+    .tab-pane{ padding:10px;}
+</style>
 @endsection
