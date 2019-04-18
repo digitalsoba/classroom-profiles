c854853e843c9b1d86cb92d7dca4dcb7568e42ae
Mohamed.O <37858811+alprince1991@users.noreply.github.com>
2019-04-18 13:57:05 -0700

    no message

--- resources/views/layout/topnavbar.blade.php
+++ resources/views/layout/topnavbar.blade.php
@@ -14,7 +14,25 @@
 <body>
 
 <div id="app">
-    <topnavbar></topnavbar>
+    <topnavbar>
+        <div class="container">
+            {!! Form::open(['route' => 'login']) !!}
+            <div class="form-group">
+                {!! Form::label('username', 'Username') !!}
+                {!! Form::text('username', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password', 'Password') !!}
+                {!! Form::password('password', ['class' => 'form-control']) !!}
+            </div>
+
+                {!! Form::submit('Submit', ['class' => 'btn btn-rounded btn-primary']) !!}
+                {!! Form::close() !!}
+            @if (session()->has('message')) <div class="alert alert-danger">{!! session('message') !!}</div>
+            @endif
+        </div>
+    </topnavbar>
 </div>
 
 <script src="{{ asset('js/app.js') }}"></script>
