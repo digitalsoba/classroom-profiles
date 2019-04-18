e1f596d4b494c324fd0d0875951a4b200b9f324f
Anthony <anthony.inlavong@gmail.com>
2019-04-18 00:02:13 -0700

    Cp 66 (#65)

---
+++ Envoy.blade.php
@@ -0,0 +1,16 @@
+@servers(['dev' => ['anthony@dev.matabit.org']])
+@servers(['prod' => ['anthony@matabit.org']])
+
+@task('update-dev', ['on' => 'dev'])
+    cd /var/www/dev/laravel
+    ls -ls
+    sudo git pull
+    sudo composer install
+@endtask
+
+@task('update-prod', ['on' => 'prod'])
+    cd /var/www/prod/laravel
+    ls -ls
+    sudo git pull
+    sudo composer install
+@endtask
